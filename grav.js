8e7de0dcaa45ea78ff7626b08400025f9add5414
wjr21 <wjr21@sussex.ac.uk>
2023-07-25 16:09:35 +0100

    Simple aspect ratio maintaining

--- grav.js
+++ grav.js
@@ -170,29 +170,26 @@ document.addEventListener('DOMContentLoaded', function () {
                 const img = new Image();
                 img.src = particle.imagePath;
                 
-                // Wait for the image to load before drawing it on the canvas
-                img.onload = () => {
-                    // Calculate the aspect ratio of the image
-                    const aspectRatio = img.width / img.height;
+                // Calculate the aspect ratio of the image
+                const aspectRatio = img.width / img.height;
 
-                    // Calculate the scaled dimensions based on particle.size and aspectRatio
-                    let width, height;
-                    if (aspectRatio >= 1) {
-                        // Image is wider than tall (landscape)
-                        width = particle.size;
-                        height = particle.size / aspectRatio;
-                    } else {
-                        // Image is taller than wide (portrait)
-                        width = particle.size * aspectRatio;
-                        height = particle.size;
-                    }
+                // Calculate the scaled dimensions based on particle.size and aspectRatio
+                let width, height;
+                if (aspectRatio >= 1) {
+                    // Image is wider than tall (landscape)
+                    width = particle.size;
+                    height = particle.size / aspectRatio;
+                } else {
+                    // Image is taller than wide (portrait)
+                    width = particle.size * aspectRatio;
+                    height = particle.size;
+                }
 
-                    // Draw the image on the canvas with the desired opacity
-                    ctx.globalAlpha = particle.isMouseOver ? 1 : 0.5;
-                    ctx.drawImage(img, particle.x - width / 2,
-                                  particle.y - height / 2, width, height);
-                    ctx.globalAlpha = 0.5;
-                };
+                // Draw the image on the canvas with the desired opacity
+                ctx.globalAlpha = particle.isMouseOver ? 1 : 0.5;
+                ctx.drawImage(img, particle.x - width / 2,
+                              particle.y - height / 2, width, height);
+                ctx.globalAlpha = 0.5;
                 
             } else {
                 // Use circle for other particles
