177da014379951b842906240c97e778e59daa962
wjr21 <wjr21@sussex.ac.uk>
2023-07-25 11:03:51 +0100

    Correcting id

--- grav.js
+++ grav.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById('canvas-overlay');
 const ctx = canvas.getContext('2d');
 
 const G = 10; // Gravitational constant
