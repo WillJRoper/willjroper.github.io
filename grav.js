8398ad53ca80757bf418105c2d0325c2384301b1
wjr21 <wjr21@sussex.ac.uk>
2023-07-25 10:59:11 +0100

    New N-body code

--- grav.js
+++ grav.js
@@ -1,162 +1,106 @@
-// script.js - N-body simulation with Newtonian gravity calculations
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
-// Get the icons
-var icon1 = document.getElementById('home');
-var icon2 = document.getElementById('about');
-// var icon3 = document.getElementById('email');
-// var icon4 = document.getElementById('twitter');
-// var icon5 = document.getElementById('github');
-// var icon6 = document.getElementById('yt');
-// var icon7 = document.getElementById('linkedin');
-// Add more icons as needed
+const G = 10; // Gravitational constant
+const numParticles = 10;
 
-// Set up the gravitational constant (adjust as needed for your simulation)
-var G = 0.01;
+// Particle class representing each element in the simulation
+class Particle {
+  constructor(x, y, mass, link, icon) {
+    this.x = x;
+    this.y = y;
+    this.mass = mass;
+    this.vx = 0;
+    this.vy = 0;
+    this.link = link;
+    this.icon = icon; // Font Awesome icon name, e.g., 'fa-bug', 'fa-star', etc.
+  }
+}
 
-// Create an array to store the icons
-var icons = [icon1, icon2];
-// var icons = [icon1, icon2, icon3, icon4, icon5, icon6, icon7];
-// Add more icons to the array as needed
+// Create an array to hold all the particles
+const particles = [];
 
-// Initialize initial velocities and accelerations to zero for all icons
-var velocities = Array.from({ length: icons.length }, () => ({ x: 0, y: 0 }));
-var accelerations = Array.from({ length: icons.length }, () => ({ x: 0, y: 0 }));
+// Initialize particles with random positions, masses, and hyperlinks
+for (let i = 0; i < numParticles; i++) {
+  const x = Math.random() * canvas.width;
+  const y = Math.random() * canvas.height;
+  const mass = 1 + Math.random() * 4; // Random mass between 1 and 5
+  const link = `https://example.com/${i}`; // Replace with the desired hyperlink
+  const icon = i % 2 === 0 ? 'fa-bug' : 'fa-star'; // Use different icons for even and odd particles
+  particles.push(new Particle(x, y, mass, link, icon));
+}
 
-// Function to set initial positions of the icons
-function setInitialPositions() {
-    var containerWidth = window.innerWidth;
-    var containerHeight = window.innerHeight;
+function update() {
+  // Clear the canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (var i = 0; i < icons.length; i++) {
-        var initialX = Math.random() * containerWidth; // Random initial X position
-        var initialY = Math.random() * containerHeight; // Random initial Y position
+  // Calculate gravitational forces and update velocities
+  for (let i = 0; i < numParticles; i++) {
+    for (let j = 0; j < numParticles; j++) {
+      if (i !== j) {
+        const particle1 = particles[i];
+        const particle2 = particles[j];
 
-        icons[i].style.left = initialX + 'px';
-        icons[i].style.top = initialY + 'px';
+        const dx = particle2.x - particle1.x;
+        const dy = particle2.y - particle1.y;
+        const distanceSq = dx * dx + dy * dy;
+        const distance = Math.sqrt(distanceSq);
 
-        velocities[i] = { x: Math.random(), y: Math.random() };
-        accelerations[i] = { x: 0, y: 0 };
+        // Calculate gravitational force
+        const force = (G * particle1.mass * particle2.mass) / distanceSq;
+
+        // Calculate components of the force
+        const fx = force * (dx / distance);
+        const fy = force * (dy / distance);
+
+        // Update velocities of the particles
+        particle1.vx += fx / particle1.mass;
+        particle1.vy += fy / particle1.mass;
+      }
     }
-}
+  }
 
-// Function to calculate the gravitational force between two icons
-function calculateGravitationalForce(icon1, icon2) {
-    var dx = icon2.offsetLeft - icon1.offsetLeft;
-    var dy = icon2.offsetTop - icon1.offsetTop;
-    var distanceSquared = dx * dx + dy * dy;
-    var forceMagnitude = G / distanceSquared;
+  // Update positions based on velocities
+  for (const particle of particles) {
+    particle.x += particle.vx;
+    particle.y += particle.vy;
 
-    var angle = Math.atan2(dy, dx);
-    var forceX = forceMagnitude * Math.cos(angle);
-    var forceY = forceMagnitude * Math.sin(angle);
-
-    return { x: forceX, y: forceY };
-}
-
-// Function to calculate the gravitational force between an icon and central mass
-function calculateGravitationalForceCentral(icon) {
-    var containerWidth = window.innerWidth;
-    var containerHeight = window.innerHeight;
-    
-    var dx = icon.offsetLeft - (containerWidth / 2);
-    var dy = icon.style.top - (containerHeight / 2);
-    var distanceSquared = dx * dx + dy * dy;
-    var forceMagnitude = G * 1000 / distanceSquared;
-
-    var angle = Math.atan2(dy, dx);
-    var forceX = forceMagnitude * Math.cos(angle);
-    var forceY = forceMagnitude * Math.sin(angle);
-
-    return { x: forceX, y: forceY };
-}
-
-// Function to update the positions of the icons using kick-drift-kick method
-function updatePositions() {
-    var containerWidth = window.innerWidth;
-    var containerHeight = window.innerHeight;
-
-    var timestep = 0.00001;
-    var halfTimestep = timestep / 2;
-    
-    for (var i = 0; i < icons.length; i++) {
-        accelerations[i].x = 0;
-        accelerations[i].y = 0;
-
-        // // Force from "black hole"
-        // var force = calculateGravitationalForceCentral(icons[i]);
-        // accelerations[i].x += force.x;
-        // accelerations[i].y += force.y;
-
-        for (var j = 0; j < icons.length; j++) {
-            if (i !== j) {
-                var force = calculateGravitationalForce(icons[i], icons[j]);
-                accelerations[i].x += force.x;
-                accelerations[i].y += force.y;
-            }
-        }
-
-        console.log(accelerations[i].x, accelerations[i].y);
-
-        velocities[i].x += 0.5 * accelerations[i].x * halfTimestep ** 2;
-        velocities[i].y += 0.5 * accelerations[i].y * halfTimestep ** 2;
-
-        console.log(velocities[i].x, velocities[i].y);
-
-        // Update icon positions with periodic boundary conditions
-        var newLeft = (icons[i].offsetLeft + (velocities[i].x * timestep)) % containerWidth;
-        var newTop = (icons[i].offsetTop + (velocities[i].y * timestep)) % containerHeight;
-        console.log(newLeft, newTop, icons[i].style.left, icons[i].style.top);
-        // Handle negative values (when the icon crosses the left or top boundary)
-        newLeft = (newLeft >= 0) ? newLeft : containerWidth + newLeft;
-        newTop = (newTop >= 0) ? newTop : containerHeight + newTop;
-
-        console.log(newLeft, newTop);
-
-        icons[i].style.left = newLeft + 'px';
-        icons[i].style.top = newTop + 'px';
-
-        console.log(icons[i].style.left, icons[i].style.top);
-
-        velocities[i].x += 0.5 * accelerations[i].x * halfTimestep ** 2;
-        velocities[i].y += 0.5 * accelerations[i].y * halfTimestep ** 2;
-
+    // Draw particles on the canvas
+    if (particle.icon) {
+      // Use Font Awesome icon
+      ctx.font = '20px FontAwesome';
+      ctx.fillStyle = 'black';
+      ctx.fillText(String.fromCharCode(parseInt('0x' + particle.icon.substring(3), 16)), particle.x, particle.y);
+    } else {
+      // Use circle for other particles
+      ctx.beginPath();
+      ctx.arc(particle.x, particle.y, 5, 0, 2 * Math.PI);
+      ctx.fillStyle = 'black';
+      ctx.fill();
     }
-    
-    requestAnimationFrame(updatePositions);
+  }
+
+  // Request the next animation frame
+  requestAnimationFrame(update);
 }
 
-// Function to show tooltip on mouseover
-function showTooltip(event) {
-    var icon = event.target.closest('.icon-link');
-    if (!icon) return;
-  
-    var tooltipText = icon.getAttribute('data-tooltip');
-    var tooltip = document.createElement('div');
-    tooltip.className = 'tooltip';
-    tooltip.textContent = tooltipText;
-    document.body.appendChild(tooltip);
-  
-    var rect = icon.getBoundingClientRect();
-    tooltip.style.top = (rect.top - 30) + 'px'; // Adjust the distance of the tooltip from the icon
-    tooltip.style.left = (rect.left + rect.width / 2) + 'px'; // Center the tooltip horizontally
-}
+// Event listener for handling clicks on particles
+canvas.addEventListener('click', function (event) {
+  const mouseX = event.clientX - canvas.getBoundingClientRect().left;
+  const mouseY = event.clientY - canvas.getBoundingClientRect().top;
 
-// Function to hide tooltip on mouseout
-function hideTooltip(event) {
-    var tooltip = document.querySelector('.tooltip');
-    if (tooltip) {
-        tooltip.remove();
+  for (const particle of particles) {
+    const dx = mouseX - particle.x;
+    const dy = mouseY - particle.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance <= 5) {
+      // If the click is inside the particle, open the associated hyperlink
+      window.open(particle.link, '_blank');
+      break; // Stop checking other particles
     }
-}
-
-// Set initial positions before starting the animation
-setInitialPositions();
-
-// Add event listeners for mouseover and mouseout events
-for (var i = 0; i < icons.length; i++) {
-    icons[i].addEventListener('mouseover', showTooltip);
-    icons[i].addEventListener('mouseout', hideTooltip);
-}
+  }
+});
 
 // Start the simulation
-updatePositions();
+update();
