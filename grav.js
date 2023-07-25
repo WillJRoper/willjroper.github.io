090366ef6c976b751a9f7a2bf07a6da7e6510488
wjr21 <wjr21@sussex.ac.uk>
2023-07-25 17:19:05 +0100

    More particles

--- grav.js
+++ grav.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
     ctx.scale(dpr, dpr);
 
     const G = 10; // Gravitational constant
-    let numParticles = 18;
+    let numParticles = 50;
 
     // Adjust this value to control the simulation speed
     const timeStep = 0.05;
