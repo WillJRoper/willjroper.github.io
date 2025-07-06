89abb7f308fd49bce9c44f024043dd3a74308cc3
wjr21 <wjr21@sussex.ac.uk>
2025-07-06 22:53:05 +0100

    Doubling the size again

--- euclid_fos_viewer/viewer.js
+++ euclid_fos_viewer/viewer.js
@@ -131,7 +131,7 @@ function renderRegions(key) {
     const vpCenter = viewer.viewport.imageToViewportCoordinates(imgPt);
 
     // 2) Build a tiny viewport‐space rect around that point
-    const vpRect = new OpenSeadragon.Rect(vpCenter.x, vpCenter.y, 0.005, 0.005);
+    const vpRect = new OpenSeadragon.Rect(vpCenter.x, vpCenter.y, 0.01, 0.01);
 
     // 3) Create your hotspot element
     const elt = document.createElement("div");
