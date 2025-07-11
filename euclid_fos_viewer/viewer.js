b7c8a63de481f8a113d897f9a5ab1f5d3d7ad95f
wjr21 <wjr21@sussex.ac.uk>
2025-07-11 07:17:12 +0100

    maybe turned off fullscreen exit?

--- euclid_fos_viewer/viewer.js
+++ euclid_fos_viewer/viewer.js
@@ -36,8 +36,6 @@ function initViewer() {
     // Show the navigator panel
     showNavigator: false,
 
-    showFullPageControl: false,
-
     // Desktop gesture settings
     gestureSettingsMouse: {
       scrollToZoom: true,
